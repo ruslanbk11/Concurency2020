632d6c2fcb6f2fb5f69bb3d170517f7c02758a6e
Ruslan <ruslanbk11@mail.ru>
2020-11-20 14:18:21 +0300

    Throughput experiment

--- src/tokenring/Main.java
+++ src/tokenring/Main.java
@@ -2,41 +2,41 @@ package tokenring;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        TokenRing tokenRing = new TokenRing(10);
+        int nodesCount = 8;
+        TokenRing tokenRing = new TokenRing(nodesCount);
         tokenRing.start();
 
-        int sum = 0;
-        for (int i = 0; i < 10; i++) {
-            tokenRing.sendMessage(0, 1, System.currentTimeMillis());
-            tokenRing.sendMessage(1, 2, System.currentTimeMillis());
-            tokenRing.sendMessage(2, 3, System.currentTimeMillis());
-            tokenRing.sendMessage(3, 4, System.currentTimeMillis());
-            tokenRing.sendMessage(4, 5, System.currentTimeMillis());
-//            tokenRing.sendMessage(5, 6, System.currentTimeMillis());
-//            tokenRing.sendMessage(6, 7, System.currentTimeMillis());
-//            tokenRing.sendMessage(7, 8, System.currentTimeMillis());
-//            tokenRing.sendMessage(8, 9, System.currentTimeMillis());
-            Thread.sleep(1000);
-            for (RingNode node : tokenRing.getNodes()) {
-                sum += node.getLatency();
-            }
-        }
-        System.out.println("Average latency: " + (sum / 50));
-//
 //        int sum = 0;
-//        tokenRing.sendMessage(0, 1, System.currentTimeMillis());
-//        tokenRing.sendMessage(1, 2, System.currentTimeMillis());
-//        tokenRing.sendMessage(2, 3, System.currentTimeMillis());
-//        tokenRing.sendMessage(3, 4, System.currentTimeMillis());
-//        tokenRing.sendMessage(4, 5, System.currentTimeMillis());
-//        tokenRing.sendMessage(5, 6, System.currentTimeMillis());
-//        tokenRing.sendMessage(6, 7, System.currentTimeMillis());
-//        tokenRing.sendMessage(7, 8, System.currentTimeMillis());
-//        tokenRing.sendMessage(8, 9, System.currentTimeMillis());
-//        Thread.sleep(1000);
-//        for (RingNode node : tokenRing.getNodes()) {
-//            sum += node.getMessagesAmount();
+//        for (int i = 0; i < 10; i++) {
+//            tokenRing.sendMessage(0, 1, System.currentTimeMillis());
+//            tokenRing.sendMessage(1, 2, System.currentTimeMillis());
+//            tokenRing.sendMessage(2, 3, System.currentTimeMillis());
+//            tokenRing.sendMessage(3, 4, System.currentTimeMillis());
+////            tokenRing.sendMessage(4, 5, System.currentTimeMillis());
+////            tokenRing.sendMessage(5, 6, System.currentTimeMillis());
+////            tokenRing.sendMessage(6, 7, System.currentTimeMillis());
+////            tokenRing.sendMessage(7, 8, System.currentTimeMillis());
+////            tokenRing.sendMessage(8, 9, System.currentTimeMillis());
+//            Thread.sleep(1000);
+//            for (RingNode node : tokenRing.getNodes()) {
+//                sum += node.getLatency();
+//            }
 //        }
-//        System.out.println("Average throughput: " + (sum / 10));
+//        System.out.println("Average latency: " + (sum / 50));
+//
+        int sum = 0;
+        tokenRing.sendMessage(0, 1, System.currentTimeMillis());
+        tokenRing.sendMessage(1, 2, System.currentTimeMillis());
+        tokenRing.sendMessage(2, 3, System.currentTimeMillis());
+        tokenRing.sendMessage(3, 4, System.currentTimeMillis());
+        tokenRing.sendMessage(4, 5, System.currentTimeMillis());
+        tokenRing.sendMessage(5, 6, System.currentTimeMillis());
+        tokenRing.sendMessage(6, 7, System.currentTimeMillis());
+        Thread.sleep(2000);
+        for (RingNode node : tokenRing.getNodes()) {
+            System.out.println(node.getMessagesAmount());
+            sum += node.getMessagesAmount();
+        }
+        System.out.println("Average throughput: " + (sum / nodesCount));
     }
 }
--- src/tokenring/RingNode.java
+++ src/tokenring/RingNode.java
@@ -5,8 +5,8 @@ public class RingNode implements Runnable {
     private RingNode next;
     private volatile Message message;
     private long latency;
-//    private int messagesAmount = 0;
-//    private long startTimestamp;
+    private int messagesAmount = 0;
+    private long startTimestamp;
 
     RingNode(int index) {
         this.index = index;
@@ -17,44 +17,47 @@ public class RingNode implements Runnable {
         this.next = next;
     }
 
+// __________FOR_LATENCY__________
+//    public void run() {
+//        while (true) {
+//            if (this.message != null) {
+//                if (this.message.getDestinationNodeIndex() != index) {
+//                    sendMessage(this.message);
+//                } else {
+//                    this.latency = (System.currentTimeMillis() - message.getDispatchTime());
+//                    System.out.println(
+//                            "Message received by RingNode" + this.index +
+//                            " Latency: " + this.latency
+//                    );
+//                }
+//                this.message = null;
+//            }
+//        }
+//    }
+
+// __________FOR_THROUGHPUT__________
     public void run() {
         while (true) {
             if (this.message != null) {
-                if (this.message.getDestinationNodeIndex() != index) {
-                    sendMessage(this.message);
-                } else {
-                    this.latency = (System.currentTimeMillis() - message.getDispatchTime());
-                    System.out.println(
-                            "Message received by RingNode" + this.index +
-                            " Latency: " + this.latency
-                    );
+                if (messagesAmount == 0) {
+                    startTimestamp = this.message.getDispatchTime();
+                }
+                sendMessage(this.message);
+                if (System.currentTimeMillis() - startTimestamp <= 1000) {
+                    messagesAmount++;
                 }
                 this.message = null;
             }
         }
     }
 
-//    public void run() {
-//        while (true) {
-//            if (message != null) {
-//                if (messagesAmount == 0) {
-//                    startTimestamp = message.getDispatchTime();
-//                }
-//                if (System.currentTimeMillis() - startTimestamp <= 1000) {
-//                    messagesAmount++;
-//                }
-//                message = null;
-//            }
-//        }
-//    }
-
     public long getLatency() {
         return latency;
     }
 
-//    public int getMessagesAmount() {
-//        return messagesAmount;
-//    }
+    public int getMessagesAmount() {
+        return messagesAmount;
+    }
 
     private void sendMessage(Message message) {
 //        System.out.println("Message sent by RingNode" + this.index);
